102255b5b58eb5c634da110bf5d3b4cfbfb17129
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-12-03 12:42:12 +0200

    Update WelcomeInViteWorld.vue

--- src/components/WelcomeInViteWorld.vue
+++ src/components/WelcomeInViteWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue'
 
-defineProps<{ msg: string }>();
+defineProps<{ msg: string }>()
 
-const count = ref(0);
+const count = ref(0)
 </script>
 
 <template>
@@ -13,27 +13,19 @@ const count = ref(0);
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
-      <code class="frame-scoped">components/WelcomeInViteWorld.vue</code> to
-      test HMR
+      <code class="frame-scoped">components/WelcomeInViteWorld.vue</code> to test HMR
     </p>
-    <p class="para-scope">
-      We're also leveraging some Added tech stacks in this Project Template
-    </p>
+    <p class="para-scope">We're also leveraging some Added tech stacks in this Project Template</p>
   </div>
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
+    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
   </p>
   <p class="read-the-docs">Click on the Logos to learn more</p>
 </template>
