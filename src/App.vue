e2f23a13cf2b135e9036377b2184fb5d02c44292
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-12-03 12:41:45 +0200

    Swapped App.vue files

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,47 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/WelcomeInViteWorld'
+import WelcomeInViteWorld from './components/WelcomeInViteWorld.vue'
+
+// Dynamic Asset Loading Where required
+function getImageUrl(name: string, ext: string): string {
+  return new URL(`./assets/images/${name}.${ext}`, import.meta.url).href
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo vite" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="./assets/typescript.svg" class="logo ts" alt="TS logo" />
+    </a>
+    <a href="https://pinia.vuejs.org/" target="_blank">
+      <img src="./assets/pinia.svg" class="logo pinia" alt="Pinia logo" />
+    </a>
+  </div>
+  <WelcomeInViteWorld msg="Vite + Vue + TS + Pinia" />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.logo.ts:hover {
+  filter: drop-shadow(0 0 2em #3179c6aa);
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.logo.pinia:hover {
+  filter: drop-shadow(0 0 2em #ffd24caa);
 }
 </style>
