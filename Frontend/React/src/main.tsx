5e106d591f4785fc0f299c9a093c189420240da1
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-12-14 12:15:39 +0200

    Create main.tsx

---
+++ Frontend/React/src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider } from '@tanstack/react-router'
+import { App } from './App'
+import { router } from './router'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
